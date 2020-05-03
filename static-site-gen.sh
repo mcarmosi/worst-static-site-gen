4f17e43ef02e4c3c194ac0e126409e0d7e85bc1d
Marco Carmosino <marco@ntime.org>
2020-05-02 20:59:31 -0700

    refactoring, by some metrics

--- static-site-gen.sh
+++ static-site-gen.sh
@@ -1,14 +1,13 @@
-#! /usr/local/bin/fish
+#!/usr/bin/env fish
 
 for i in (find . -name \*.md -type f)
     echo (dirname $i)/(basename $i .md).html
-    pandoc -s -o (dirname $i)/(basename $i .md).html $i
+    pandoc --css site.css -s -o (dirname $i)/(basename $i .md).html $i
 end
 
 function ytojs
     # slice out YAML front-matter, convert to JSON
-    sed '1d; /---/q' $argv | sed '$d' | read -z yaml
-    echo $yaml | yq r -j /dev/stdin
+    sed '1d; /---/q' $argv | sed '$d' | yq r -j /dev/stdin
 end
 
 function tocgather
@@ -16,8 +15,7 @@ function tocgather
     for i in (find articles -name \*.md -type f)[-1..1]
 	set target (basename $i .md).html
 	ytojs $i | jq ".file = \"$target\"" | read -z jsonblob
-	cat toc.json | jq ".article += [$jsonblob]" | read -z toci
-	echo $toci >toc.json
+	cat toc.json | jq ".article += [$jsonblob]" >toc.json
     end
 end
 
@@ -28,6 +26,23 @@ end
 
 tocgather
 tocbuild
+
+
+## echo "$(sort your_file)" > your_file
+# above: a one-liner to modify files in place
+
+# cat toc.json | jq ".article += [(ytojs $i | jq ".file = \"$target\"")]" >toc.json
+# but you've got to work out the right quoting in that middle bit
+# actually what is probably better
+# function get_blob
+#     ytojs $argv[0] | jq ".file = \"$argv[1]\""
+# end
+
+# cat toc.json | jq ".article += [(get_blob)]" > toc.json
+# that is, if you wanted to factor that out
+# which I don't think is needed
+# but that's what it would look like
+# you don't need to read toci at any rate
 
 # blog.json is where "global variables" go
 # nb: currently there is no blog.json
